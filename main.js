c8caddbf2b135022396ffe9f56981d6d324f4a72
Lennie Nurse <lnursej@gmail.com>
2022-08-23 18:37:36 -0400

    "passed 2/2"

--- main.js
+++ main.js
@@ -7,3 +7,24 @@ for (let i = 0; i < 100; i++) {
 }
 
 // You may write your code here!
+
+const color = document.querySelectorAll(".color");
+const current = document.querySelector("#current-color");
+
+color.forEach((cur) => {
+  cur.addEventListener("click", (event) => {
+    // console.log(event)
+    // console.log(color)
+    current.style.backgroundColor = event.target.style.backgroundColor;
+  });
+});
+
+const cell = document.querySelectorAll(".cell");
+// console.log(cell);
+
+cell.forEach((element) => {
+  element.addEventListener("click", (event) => {
+    // console.log(event);
+    event.target.style.backgroundColor = current.style.backgroundColor;
+  });
+});
